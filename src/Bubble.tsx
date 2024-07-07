c4b4f5ef2293e053f762938a464d15991e69dfcd
kiddogum <123975377+kiddogum@users.noreply.github.com>
2024-07-07 15:07:31 +0700

    optimized for build

--- src/Bubble.tsx
+++ src/Bubble.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type BubbleProps = {
   text: string;
   author: string;
--- src/Cards.tsx
+++ src/Cards.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type CardsProps = {
   image: string;
   food: string;
