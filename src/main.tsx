a91d15737bab016c3eb3824367c3f917d3f9b8b5
kiddogum <123975377+kiddogum@users.noreply.github.com>
2024-07-07 09:11:24 +0700

    added base

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<h1>hello</h1>);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <h1 className="font-hiro text-3xl">hello</h1>
+);
